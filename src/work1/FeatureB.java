029dd3f13001ba09e82ee12446a504ef2c8d5286
ElChern <chernikhlena@gmail.com>
2022-09-01 11:56:47 -0400

    Added more to branch b

--- src/work1/FeatureB.java
+++ src/work1/FeatureB.java
@@ -4,5 +4,6 @@ public class FeatureB {
     public static void main(String[] args) {
         System.out.println("Created branch 2");
         System.out.println("Smth");
+        System.out.println("Need to add something to feature-b branch");
     }
 }
