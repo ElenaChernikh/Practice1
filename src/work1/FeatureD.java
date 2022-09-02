c81e4b66d67aee38bd5e31852ee282dfe22fbd71
ElChern <chernikhlena@gmail.com>
2022-09-02 10:28:51 -0400

    more to commit

--- src/work1/FeatureD.java
+++ src/work1/FeatureD.java
@@ -4,6 +4,7 @@ public class FeatureD {
     public static void main(String[] args) {
         System.out.println("Created new branch from Master file and called it feature-d");
         System.out.println("something new");
+        System.out.println("A little bit more to commit");
 
     }
 }
