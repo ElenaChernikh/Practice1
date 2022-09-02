7d65c5ca71e972662bb7e23e6fe2ce702a97177c
ElChern <chernikhlena@gmail.com>
2022-09-02 10:28:25 -0400

    something new

--- src/work1/FeatureD.java
+++ src/work1/FeatureD.java
@@ -3,5 +3,7 @@ package work1;
 public class FeatureD {
     public static void main(String[] args) {
         System.out.println("Created new branch from Master file and called it feature-d");
+        System.out.println("something new");
+
     }
 }
