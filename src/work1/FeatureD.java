cef04a084fdbd8ad2ba3f9a360e4077b95fc74ea
ElChern <chernikhlena@gmail.com>
2022-09-02 10:27:01 -0400

    Created branch feature-d

---
+++ src/work1/FeatureD.java
@@ -0,0 +1,7 @@
+package work1;
+
+public class FeatureD {
+    public static void main(String[] args) {
+        System.out.println("Created new branch from Master file and called it feature-d");
+    }
+}
